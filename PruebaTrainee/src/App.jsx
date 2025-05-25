b9dd8d04a7a0abe9cea402b60c2d1bce5d80044c
Jean Melgar <jean041519@gmail.com>
2025-05-25 03:58:45 -0500

    app.css

--- PruebaTrainee/src/App.jsx
+++ PruebaTrainee/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import './App.css'
+import './app.css'
 // se soluciono el problema de el nombre del app a la hora de hacer build
 import { Getfact } from './services/serviceFact'
 
