8c9d943baebdf496d01bc159f30aadac1893f4ef
Jean Melgar <jean041519@gmail.com>
2025-05-25 04:41:26 -0500

    se agrego el paquete de analiticas de vercel v0.1

--- PruebaTrainee/src/main.jsx
+++ PruebaTrainee/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App.jsx'
-
+import { Analytics } from '@vercel/analytics/next'
 const root = createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(<><App /><Analytics /></>)
